ecfe48b53fd3b1debaadd110513f293e4ac6debe
govinda sah <sahgovinda401@gmail.com>
2025-08-30 07:01:05 +0530

    Initial commit

---
+++ adminpage/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . views import *
+
+urlpatterns = [
+    path("",adminhome, name='admins'),
+    path("productlist/",productlist, name='productlist'),
+    path('addproduct/',addproduct,name='addproduct'),
+    path('categorylist/',categorylist,name='categorylist'),
+    path('addcategory/',addcategory, name='addcategory'),
+    path('updateproduct/<int:product_id>',updateproduct, name='updateproduct'),
+    path('deleteproduct/<int:product_id>', deleteproduct,name='deleteproduct'),
+    
+]
